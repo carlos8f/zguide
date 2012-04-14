6e63655d379557ce5fe7f67ac6d82f35a8c2548d
Carlos Rodriguez <carlos@s8f.org>
2012-04-13 18:38:23 -0700

    Playing with benchmarking pub/sub.

--- examples/Node.js/publisher.js
+++ examples/Node.js/publisher.js
@@ -4,15 +4,42 @@ var publisher = zmq.socket('pub')
 publisher.bind('tcp://*:8688', function(err) {
   if(err)
     console.log(err)
-  else
+  else {
     console.log("Listening on 8688...")
+  }
 })
 
-for (var i=1 ; i<10 ; i++)
-    setTimeout(function() {
-        console.log('sent');
-        publisher.send("Hello there!")
-    }, 1000 * i)
+/*
+var x = 100;
+setTimeout(function() {
+  setInterval(function() {
+    for(var i = 0; i < x; i++) {
+        var time = new Date().getTime();
+        publisher.send(time);
+    }
+    console.log('sent ' + x + ' messages')
+    //x = x * 2;
+  }, 50);
+}, 1000)
+*/
+
+var sent = 0;
+setInterval(function() {
+  console.log(sent + "/sec");
+  sent = 0;
+}, 1000)
+
+function doTheThing() {
+  //var time = new Date().getTime();
+  publisher.send(1);
+  publisher.send(1);
+  sent += 2;
+  process.nextTick(doTheThing);
+}
+
+setTimeout(function() {
+  doTheThing();
+}, 1000)
 
 process.on('SIGINT', function() {
   publisher.close()
--- examples/Node.js/subscriber.js
+++ examples/Node.js/subscriber.js
@@ -1,8 +1,16 @@
 var zmq = require('zmq')
 var subscriber = zmq.socket('sub')
 
-subscriber.on("message", function(reply) {
-  console.log('Received message: ', reply.toString());
+var got = 0;
+
+setInterval(function() {
+  console.log(got + "/sec")
+  got = 0;
+}, 1000)
+
+subscriber.on("message", function(time) {
+  //console.log(new Date().getTime() - time)
+  got++;
 })
 
 subscriber.connect("tcp://localhost:8688")
